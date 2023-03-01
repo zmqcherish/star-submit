07451b2aecdffb55eeb9b5153d1877f19e31ebfb
cherish <cherishzhang@sohu-inc.com>
2023-03-01 15:21:54 +0800

    init

--- src/background.ts
+++ src/background.ts
@@ -13,8 +13,8 @@ protocol.registerSchemesAsPrivileged([
 async function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 400,
+    height: 300,
     webPreferences: {
       
       // Use pluginOptions.nodeIntegration, leave this alone
