b66601436e32cd5270c97d3e7f2e8fd0cc50afad
cherish <cherishzhang@sohu-inc.com>
2023-03-02 16:06:32 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import '../static/main.css'
-import '../static/icon.css'
 
 createApp(App).use(store).use(router).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,18 @@
 <template>
-  <div>
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-	<cu-input v-model="pValue"
-				title="item"
-				placeholder="请输入"
-			/>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import CuInput from '@/components/CuInput.vue'; // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-	CuInput
+    HelloWorld,
   },
-  data() {
-            return {
-                pValue: '11'
-            }
-        }
 });
 </script>
